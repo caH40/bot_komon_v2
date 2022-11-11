d489cc600e4d83b67e1e45647b78233dca93cab6
caH40 <kmvsancho@gmail.com>
2022-11-11 20:45:18 +0300

    fixed bug with time results

--- utility/thousandths-seconds.js
+++ utility/thousandths-seconds.js
@@ -1,6 +1,12 @@
 export function filterThousandths(results) {
 	try {
 		const lengthArr = results.length;
+		let first = results[0].time;
+		let last = results[lengthArr - 1].time;
+
+		if (first.split('.')[0] !== results[1].time.split('.')[0]) first = first.split('.')[0];
+
+		if (last.split('.')[0] !== results[lengthArr - 2].time.split('.')[0]) last = last.split('.')[0];
 
 		for (let i = 1; i < lengthArr; i++) {
 			let timePrev = '';
@@ -16,6 +22,9 @@ export function filterThousandths(results) {
 				results[i].time = timeCurrent;
 			}
 		}
+
+		if (results[lengthArr - 1].time.split('.')[0] !== results[lengthArr - 2].time.split('.')[0])
+			results[lengthArr - 1].time = results[lengthArr - 1].time.split('.')[0];
 	} catch (error) {
 		console.log(error);
 	}
