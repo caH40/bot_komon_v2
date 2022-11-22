9e3cb76af692bed2caf80cebb5c31edfb026087e
caH40 <kmvsancho@gmail.com>
2022-11-22 12:33:05 +0300

    changed notice module

--- modules/notice.js
+++ modules/notice.js
@@ -1,23 +1,60 @@
+import { Click } from '../Model/Click.js';
 import { Rider } from '../Model/Rider.js';
 import { Stage } from '../Model/Stage.js';
 
+export async function getUsersForSpam(theme) {
+	try {
+		// news,newRace,botInfo,training
+		const ridersDB = await Rider.find();
+		const ridersWishNotice = await Rider.find({ [`settings.notice.${theme}`]: true });
+		const ridersClickingDB = await Click.find();
+
+		let telegramIdsRider = [];
+		ridersDB.forEach(rider => telegramIdsRider.push(rider.telegramId));
+
+		let telegramIdsRiderClicking = [];
+		ridersClickingDB.forEach(rider => telegramIdsRiderClicking.push(rider.user.id));
+
+		const usersFiltered = telegramIdsRiderClicking.filter(
+			telegramId => !telegramIdsRider.includes(telegramId)
+		);
+
+		let telegramIdsRiderWishNotice = [];
+		ridersWishNotice.forEach(rider => telegramIdsRiderWishNotice.push(rider.telegramId));
+
+		const users = [...usersFiltered, ...telegramIdsRiderWishNotice];
+
+		return users;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
 export async function noticeGetResult(ctx, protocol) {
 	try {
-		const ridersDB = await Rider.find({ 'settings.notice.botInfo': true });
+		const users = await getUsersForSpam('botInfo', protocol);
 		const stageDB = await Stage.findOne({ _id: protocol.stageId }).populate('seriesId');
 
-		ridersDB.forEach((rider, index) => {
-			let zwiftIdCur = protocol.results.find(result => +result.zwiftId === rider.zwiftId)?.zwiftId;
+		//массив с telegramID райдеров, принимавших участие в заезде
+		const ridersDB = await Rider.find();
+		let telegramIdRidersInProtocol = [];
+		protocol.results.forEach(result => {
+			telegramIdRidersInProtocol.push(
+				ridersDB.find(rider => +result.zwiftId === rider.zwiftId)?.telegramId
+			);
+		});
 
-			let subMessage = zwiftIdCur ? 'в котором Вы принимали участие.' : '';
+		users.forEach((telegramId, index) => {
+			let subMessage = telegramIdRidersInProtocol.includes(telegramId)
+				? 'в котором Вы принимали участие.'
+				: '';
 			let message = `${new Date().toLocaleString()}. Загружены результаты этапа №${
 				stageDB.number
 			} ${stageDB.seriesId.name} ${subMessage} 📋`;
-
 			setTimeout(async () => {
 				await ctx.telegram
-					.sendMessage(rider.telegramId, message)
-					.catch(error => console.log(error));
+					.sendMessage(telegramId, message)
+					.catch(error => console.log('Не найден chatId'));
 			}, index * 30);
 		});
 	} catch (error) {
