5a2708ad633f49e1854b435892a51884050e4aff
Alexander Berezhnev <freelancerbav@gmail.com>
2022-12-02 23:15:44 +0300

    changed the rule category

--- utility/category-rule.js
+++ utility/category-rule.js
@@ -4,25 +4,9 @@ export function ruleCategory(watt, wattPerKg, gender) {
 	try {
 		if (gender === 'женский') return 'W';
 
-		if (
-			(watt > 279 && wattPerKg > 3.84) ||
-			(watt > 289 && wattPerKg > 3.74) ||
-			(watt > 299 && wattPerKg > 3.64) ||
-			wattPerKg > 3.99
-		)
-			return 'A';
-
-		if (
-			(watt > 259 && wattPerKg > 2.89) ||
-			(watt > 249 && wattPerKg > 2.99) ||
-			(watt > 239 && wattPerKg > 3.09) ||
-			wattPerKg > 3.19
-		)
-			return 'B';
-
-		if (watt > 100) return 'C';
-
-		return 'D';
+		if (watt > 249 && wattPerKg > 3.99) return 'A';
+		if (watt > 200 && wattPerKg > 3.19) return 'B';
+		return 'C';
 	} catch (error) {
 		console.log(error);
 	}
