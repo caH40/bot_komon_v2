745ecd542255caec9085a7bd63e3f9d27f4cc19a
Alexander Berezhnev <freelancerbav@gmail.com>
2022-12-02 23:17:53 +0300

    changed the rule category

--- utility/category-rule.js
+++ utility/category-rule.js
@@ -1,13 +1,6 @@
-// Группа D переходит в С
-
 export function ruleCategory(watt, wattPerKg, gender) {
-	try {
-		if (gender === 'женский') return 'W';
-
-		if (watt > 249 && wattPerKg > 3.99) return 'A';
-		if (watt > 200 && wattPerKg > 3.19) return 'B';
-		return 'C';
-	} catch (error) {
-		console.log(error);
-	}
+	if (gender === 'женский') return 'W';
+	if (watt > 249 && wattPerKg > 3.99) return 'A';
+	if (watt > 199 && wattPerKg > 3.19) return 'B';
+	return 'C';
 }
