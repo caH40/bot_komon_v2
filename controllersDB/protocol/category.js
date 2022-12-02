7965fb2c31acb3d464a4236ad396be717b5380b4
Alexander Berezhnev <freelancerbav@gmail.com>
2022-12-02 23:30:21 +0300

    changed updating a category after upload a protocol

--- controllersDB/protocol/category.js
+++ controllersDB/protocol/category.js
@@ -4,19 +4,20 @@ import { Stage } from '../../Model/Stage.js';
 
 export async function updateCategoryDB(seriesId, result, categoryCurrent) {
 	try {
-		const riderDB = await Rider.findOne({ zwiftId: result.zwiftId });
-		//если аккаунт существует, то выход
-		if (riderDB) return { category: riderDB.category };
+		// закомментировал для первого этапа
+		// const riderDB = await Rider.findOne({ zwiftId: result.zwiftId });
+		// //если аккаунт существует, то выход
+		// if (riderDB) return { category: riderDB.category };
 
-		const stageDB = await Stage.find({ seriesId });
+		// const stageDB = await Stage.find({ seriesId });
 
-		for (let i = 0; i < stageDB.length; i++) {
-			const riderZwiftIdDB = await Result.findOne({
-				stageId: stageDB[i],
-				zwiftRiderId: result.zwiftId,
-			});
-			if (riderZwiftIdDB) return { category: riderZwiftIdDB.category };
-		}
+		// for (let i = 0; i < stageDB.length; i++) {
+		// 	const riderZwiftIdDB = await Result.findOne({
+		// 		stageId: stageDB[i],
+		// 		zwiftRiderId: result.zwiftId,
+		// 	});
+		// 	if (riderZwiftIdDB) return { category: riderZwiftIdDB.category };
+		// }
 
 		return { category: categoryCurrent };
 	} catch (error) {
