f0712786cbb75a3553ac6ec875a008ebe5ae7df0
Alexander Berezhnev <freelancerbav@gmail.com>
2022-12-09 17:36:25 +0300

    changed a counting clicks2

--- Model/Click.js
+++ Model/Click.js
@@ -2,6 +2,7 @@ import pkg from 'mongoose';
 const { Schema, model } = pkg;
 
 const clickSchema = new Schema({
+	_id: false,
 	user: { type: Object, unique: true, required: true },
 	clicksPerDay: [{ date: Number, clicks: Number }],
 });
