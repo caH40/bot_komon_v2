174ead181fe559195dabf6efe9a0bbf1bf3dcad0
Alexander Berezhnev <freelancerbav@gmail.com>
2022-12-21 13:06:20 +0300

    fixed the bug with new user

--- Model/Click.js
+++ Model/Click.js
@@ -2,9 +2,8 @@ import pkg from 'mongoose';
 const { Schema, model } = pkg;
 
 const clickSchema = new Schema({
-	_id: false,
 	user: { type: Object, unique: true, required: true },
-	clicksPerDay: [{ date: Number, clicks: Number }],
+	clicksPerDay: [{ date: Number, clicks: Number, _id: false }],
 });
 
 export const Click = model('Click', clickSchema);
