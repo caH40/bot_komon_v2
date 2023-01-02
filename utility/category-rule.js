94cfe2b2e0ad0573bac1dc9968f3ed655f574fcc
Alexander Berezhnev <freelancerbav@gmail.com>
2023-01-02 18:48:50 +0300

    changed the rule category - 0.9 ratio

--- utility/category-rule.js
+++ utility/category-rule.js
@@ -1,8 +1,17 @@
 export function ruleCategory(watt, wattPerKg, gender, type) {
 	if (type === 'tour') {
-		if (gender === 'женский' && wattPerKg > 3.69 && (watt > 250 || watt === 250)) return 'WA';
+		if (
+			gender === 'женский' &&
+			(wattPerKg * 0.9 > 3.7 || wattPerKg * 0.9 === 3.7) &&
+			(watt * 0.9 > 250 || watt * 0.9 === 250)
+		)
+			return 'WA';
 		if (gender === 'женский') return 'WB';
-		if ((watt * 0.95 > 250 || watt * 0.95 === 250) && wattPerKg * 0.95 > 3.99) return 'A';
+		if (
+			(watt * 0.9 > 250 || watt * 0.9 === 250) &&
+			(wattPerKg * 0.9 > 4 || wattPerKg * 0.9 === 4)
+		)
+			return 'A';
 		return 'B';
 	}
 
