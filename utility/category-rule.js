c7f8a0c4e3da60d3efc05ed2e34537750ed5f895
Alexander Berezhnev <freelancerbav@gmail.com>
2023-01-02 09:30:21 +0300

    added the rule tour category

--- utility/category-rule.js
+++ utility/category-rule.js
@@ -1,4 +1,11 @@
-export function ruleCategory(watt, wattPerKg, gender) {
+export function ruleCategory(watt, wattPerKg, gender, type) {
+	if (type === 'tour') {
+		if (gender === 'женский' && wattPerKg > 3.69 && (watt > 250 || watt === 250)) return 'WA';
+		if (gender === 'женский') return 'WB';
+		if ((watt * 0.95 > 250 || watt * 0.95 === 250) && wattPerKg * 0.95 > 3.99) return 'A';
+		return 'B';
+	}
+
 	if (gender === 'женский' && wattPerKg > 2.99) return 'WA';
 	if (gender === 'женский' && wattPerKg < 3) return 'WB';
 	if (watt * 0.97 > 249 && wattPerKg * 0.97 > 3.99) return 'A';
