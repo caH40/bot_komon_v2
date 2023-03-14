d286e7e8d85c4a40c340e7cfbc325af4c0434d1c
Alexander Berezhnev <freelancerbav@gmail.com>
2023-03-14 17:58:47 +0300

    added authorization

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import cors from 'cors';
 import express from 'express';
 import path from 'path';
+import cookieParser from 'cookie-parser';
+
 import { routerAuth } from './routes/authentication.js';
 import { router } from './routes/routes.js';
 
@@ -16,8 +18,10 @@ export async function serverExpress() {
 			origin: process.env.FRONT,
 		})
 	);
+	app.use(express.json({ limit: '5mb' }));
 
-	app.use(express.json());
+	app.use(cookieParser());
+
 	app.use(express.static(path.resolve(__dirname, 'build')));
 
 	app.use(router);
