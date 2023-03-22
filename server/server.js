334e5a57fcecc85001daff466b9230c038d84ee0
Alexander Berezhnev <freelancerbav@gmail.com>
2023-03-22 16:55:41 +0300

    remove zp-routes, controlers

--- server/server.js
+++ server/server.js
@@ -3,9 +3,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 
-import { routerAuth } from './routes/authentication.js';
 import { router } from './routes/routes.js';
-import { routerZP } from './routes/routesZP.js';
 
 const __dirname = path.resolve();
 
@@ -14,20 +12,10 @@ export async function serverExpress() {
 
 	const app = express();
 	app.use(cors());
-	// app.use(
-	// 	cors({
-	// 		credentials: true,
-	// 		origin: process.env.FRONT,
-	// 	})
-	// );
 	app.use(express.json({ limit: '5mb' }));
-
 	app.use(cookieParser());
-
 	app.use(express.static(path.resolve(__dirname, 'build')));
-
 	app.use(router);
-	app.use(routerZP);
-	app.use('/api/auth', routerAuth);
+
 	app.listen(PORT, () => console.log('server started on PORT=' + PORT));
 }
