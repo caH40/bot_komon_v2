e96d0360fa016db0dffb42d8e50756b71076e8c4
Alexander Berezhnev <freelancerbav@gmail.com>
2023-09-28 08:09:10 +0300

    turned off all the fitches

--- bot-komon.js
+++ bot-komon.js
@@ -2,18 +2,18 @@ import 'dotenv/config';
 import { session, Telegraf } from 'telegraf';
 import mongoose from 'mongoose';
 
-import { start } from './controllers/start.js';
-import { help } from './controllers/help.js';
-import { mainMenu } from './controllers/main.js';
-import { callbackQuery } from './controllers/callback-query.js';
-import { filterMessage } from './middleware/filter-message.js';
-import { activationScenes } from './scenes/activation-scenes.js';
-import { countClick } from './middleware/count-click.js';
-import { getClicks } from './controllers/clicks.js';
-import { nodeSchedule } from './modules/node-schedule.js';
+// import { start } from './controllers/start.js';
+// import { help } from './controllers/help.js';
+// import { mainMenu } from './controllers/main.js';
+// import { callbackQuery } from './controllers/callback-query.js';
+// import { filterMessage } from './middleware/filter-message.js';
+// import { activationScenes } from './scenes/activation-scenes.js';
+// import { countClick } from './middleware/count-click.js';
+// import { getClicks } from './controllers/clicks.js';
+// import { nodeSchedule } from './modules/node-schedule.js';
 import { serverExpress } from './server/server.js';
-import { readChannelPosts } from './modules/channel.js';
-import { sendNotice } from './controllers/notice-post.js';
+// import { readChannelPosts } from './modules/channel.js';
+// import { sendNotice } from './controllers/notice-post.js';
 
 await mongoose
   .connect(process.env.MONGODB)
@@ -24,22 +24,23 @@ await serverExpress().catch((error) => console.log(error));
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const stage = activationScenes();
+// const stage = activationScenes();
 
-bot.use(session());
-bot.use(stage.middleware());
-bot.use(filterMessage);
-bot.use(countClick);
+// bot.use(session());
+// bot.use(stage.middleware());
+// bot.use(filterMessage);
+// bot.use(countClick);
 
-bot.command('start', async (ctx) => await start(ctx));
-bot.command('help', async (ctx) => await help(ctx));
-bot.command('main', async (ctx) => await mainMenu(ctx));
-bot.command('click', async (ctx) => await getClicks(ctx));
-bot.on('callback_query', async (ctx) => await callbackQuery(ctx));
-bot.on('channel_post', async (ctx) => await readChannelPosts(ctx, ctx.update.channel_post));
-bot.on('message', async (ctx) => await sendNotice(ctx));
+// bot.command('start', async (ctx) => await start(ctx));
+// bot.command('help', async (ctx) => await help(ctx));
+// bot.command('main', async (ctx) => await mainMenu(ctx));
+// bot.command('click', async (ctx) => await getClicks(ctx));
+// bot.on('callback_query', async (ctx) => await callbackQuery(ctx));
+// bot.on('channel_post', async (ctx) => await readChannelPosts(ctx, ctx.update.channel_post));
+// bot.on('message', async (ctx) => await sendNotice(ctx));
 
-bot.launch().then(nodeSchedule());
+// bot.launch().then(nodeSchedule());
+bot.launch();
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
